01494cdf1ecff30239a4ccfb70066373e7763229
Liguojun <937135918@qq.com>
2020-09-05 11:33:28 +0800

    工程构建

---
+++ shopmall/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2, // tab缩进大小,默认为2
+  semi: false, // 使用分号, 默认true
+  singleQuote: true, // 使用单引号, 默认false
+  trailingComma: 'es5', // 行尾逗号,默认none,可选 none|es5(包括es5中的数组、对象)|all(包括函数对象等所有可选)
+  bracketSpacing: true, // 在对象文字中的括号之间打印空格
+  arrowParens: 'always', // 箭头函数参数括号 默认avoid 可选 avoid(能省略括号的时候就省略 例如x => x)| always(总是有括号)
+}
